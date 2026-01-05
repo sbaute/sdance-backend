4a0d93738dd142fc50b26e41bdedcddf93eeae9a
sbaute <sbaute17@gmail.com>
2026-01-05 18:05:16 -0300

    fix secret key

--- src/main/java/com/sdance_backend/sdance/service/auth/JwtService.java
+++ src/main/java/com/sdance_backend/sdance/service/auth/JwtService.java
@@ -15,31 +15,29 @@ import java.util.Map;
 @Service
 public class JwtService {
 
-    private final SecretKey secretKey;
-    private final Long expirationInMinutes;
+    @Value("${security.jwt.expiration-in-minutes}")
+    private Long EXPIRATION_IN_MINUTES;
 
-    public JwtService(
-            @Value("${security.jwt.secret-key}") String secretKey,
-            @Value("${security.jwt.expiration-in-minutes}") Long expirationInMinutes
-    ) {
-        this.secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(secretKey));
-        this.expirationInMinutes = expirationInMinutes;
-    }
+    @Value("${security.jwt.secret-key}")
+    private String SECRET_KEY;
 
     public String generateToken(UserDetails user, Map<String, Object> extraClaims) {
         Date issuedAt = new Date(System.currentTimeMillis());
-        Date expiration = new Date(
-                issuedAt.getTime() + expirationInMinutes * 60 * 1000
-        );
+        Date expiration = new Date(issuedAt.getTime() + EXPIRATION_IN_MINUTES * 60 * 1000);
 
-        return Jwts.builder()
+        String jwt = Jwts.builder()
                 .subject(user.getUsername())
                 .issuedAt(issuedAt)
                 .expiration(expiration)
                 .claims(extraClaims)
-                .signWith(secretKey, Jwts.SIG.HS256)
+                .signWith(secretKey, Jwts.SIG.HS256)   // firma
                 .compact();
+
+        return jwt;
     }
+
+    //Se genera una sola vez
+    private final SecretKey secretKey = Keys.hmacShaKeyFor(Decoders.BASE64.decode(SECRET_KEY));
 
     public String extractUsername(String jwt) {
         return parseToken(jwt).getSubject();
@@ -52,4 +50,6 @@ public class JwtService {
                 .parseSignedClaims(jwt)
                 .getPayload();
     }
+
+
 }
