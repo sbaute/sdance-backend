89c1cfccf2f8ed7af1361b2a8feb9a8b21bb2e05
sbaute <sbaute17@gmail.com>
2026-01-05 18:28:46 -0300

    add route public

--- src/main/java/com/sdance_backend/sdance/config/security/HttpSecurityConfig.java
+++ src/main/java/com/sdance_backend/sdance/config/security/HttpSecurityConfig.java
@@ -35,6 +35,7 @@ public class HttpSecurityConfig {
                 .authorizeHttpRequests(auth -> {
 
                     // Rutas de autenticación
+                    auth.requestMatchers(HttpMethod.POST, "/api/v1/user/register").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "/api/v1/auth/authenticate").permitAll();
                     auth.requestMatchers(HttpMethod.POST, "/api/v1/auth/validate").permitAll();
 
